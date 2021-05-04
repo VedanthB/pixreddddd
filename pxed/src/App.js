989996de9d747d31ff26799280f1d39dfaf4995e
vedanth bora <75097551+VedanthB@users.noreply.github.com>
2021-05-04 13:12:15 +0530

    ADD : editor and css

--- pxed/src/App.js
+++ pxed/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Header from './components/Header';
+import TextEditor from './components/TextEditor';
 
 function App() {
   return (
     <div className="app">
        <Header /> 
+       <div className='app__body'>
+         <TextEditor />
+       </div>
     </div>
   );
 }
